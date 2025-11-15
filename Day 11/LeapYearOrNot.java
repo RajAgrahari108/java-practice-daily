e08591a81e5f4cf218de3b65782e8fdc1eec0432
Raj Agrahari <raj.agr2002@gmail.com>
2025-11-15 23:46:51 +0530

    LeapYearOrNot updating

--- Day 11/LeapYearOrNot.java
+++ Day 11/LeapYearOrNot.java
@@ -4,7 +4,7 @@ public class LeapYearOrNot {
         if((year % 4==0 && year %100 !=0) ||( year %400 ==0)){
             System.out.println("Year is leap year");
         }else{
-            System.out.print("year is not a leap year");
+            System.out.println("year is not a leap year");
         }
     }
 }
