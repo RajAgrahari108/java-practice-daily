9c9c672d12d43568d34811e7aa4d77dec50b6d89
Raj Agrahari <raj.agr2002@gmail.com>
2025-12-13 23:57:01 +0530

    Adding

--- Day 12/TriangleType.java
+++ Day 12/TriangleType.java
@@ -15,7 +15,7 @@ public class TriangleType {
         }else if(a==b || b==c || a==c){
             System.out.println("the triangle is Isosceles");
         }else{
-            System.out.println("the triangle is Scalene.");
+            ystem.out.println("the triangle is Scalene.");
         }
 
     }
