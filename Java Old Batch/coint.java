bd5c4ea01400cf8d050a3b62c8b126b99b43f26e
Raj Agrahari <raj.agr2002@gmail.com>
2025-12-17 00:51:01 +0530

    Adding

---
+++ Java Old Batch/coint.java
@@ -0,0 +1,12 @@
+public class coint {
+    public static void main(String[] args) {
+        int n=10; int count=1;
+        for(int i=1; i<=n; i++){
+        if(n%i==0){
+            count++;
+        }
+        }
+        // count++;
+        System.out.println(count);
+    }
+}
