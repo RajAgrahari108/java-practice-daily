b554fb2c42956432e560659b7223504249dac099
Raj Agrahari <raj.agr2002@gmail.com>
2026-01-05 11:55:18 +0530

    All Pattern code in java For practice

---
+++ Programing Batch  Sameer sir/Pattern/Day 3/Ten.java
@@ -0,0 +1,11 @@
+// public class Ten {
+//     public static void main(String[] args) {
+//         int n = 5;
+//         for(int i=1; i<=n; i++){
+//             for(int j=1; j<=i;j++){
+//                 System.out.print((char)(i + 95) + " ");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
