12bd8f657e1ae31da4915713c324b53101d7f09c
Raj Agrahari <raj.agr2002@gmail.com>
2026-01-07 15:45:37 +0530

    code updating in a pattern

---
+++ Programing Batch  Sameer sir/Pattern/Day 4/AnotherNinteen.java
@@ -0,0 +1,21 @@
+public class AnotherNinteen {
+    public static void main(String[] args) {
+        int n = 5;
+        for(int i=1; i<=n; i++){
+            for(int k=1; k<=n-i; k++){
+            System.out.print("  ");
+
+            }
+            int x=1;
+            for(int j=1; j<=(2 * i - 1); j++){
+                System.out.print(x +" ");
+                if(j<=i-1){
+                    x++;
+                }else{
+                    x--;
+                }
+            }
+                System.out.println();
+        }
+    }
+}
